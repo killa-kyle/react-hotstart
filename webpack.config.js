bcc3baa595251b8286543cbeef29235be6d6275a
killa-kyle <kylerose@gmail.com>
2016-06-22 12:50:57 -0400

    build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var buildConfig = require('hjs-webpack')
 
 var config = buildConfig({
   in: 'src/index',
-  out: 'dist',
+  out: 'public',
   clearBeforeBuild: true
 })
 config.entry.unshift('react-hot-loader/patch')
